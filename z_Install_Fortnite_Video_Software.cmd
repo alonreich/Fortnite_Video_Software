7836f99ecd962cac3f8c86a30fca5f2206771cc8
unknown <alon.reich@gmail.com>
2025-10-30 13:58:10 +0200

    sync

--- z_Install_Fortnite_Video_Software.cmd
+++ z_Install_Fortnite_Video_Software.cmd
@@ -1,1 +1,2 @@
-powershell.exe -ep bypass -c "iex(New-Object Net.WebClient).DownloadString('https://github.com/alonreich/Fortnite_Video_Software/blob/main/Installer.ps1')"
+powershell.exe -ep bypass -c "iex(New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/alonreich/Fortnite_Video_Software/main/Installer.ps1')"
+pause
